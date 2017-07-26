3e71cf6bc17b23a6d99f306ce5439043314bace7
mayankasar <mayankasar16@gmail.com>
2017-07-26 23:23:48 +0530

    Merge Reolve

--- CommandPrompt/src/main/Main.java
+++ CommandPrompt/src/main/Main.java
@@ -6,7 +6,6 @@ public class Main
     public static void main(String args[])
     {
         //ONLINE COMMENT ADDED
-        System.out.println("Hellow GitHub WORLD");
-        //ANOTHER COMMENT :)
+        System.out.println("Hellow GitHub");
     }
 }

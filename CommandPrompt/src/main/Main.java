45c009e50115947efc6b5f6a55eff876b1278985
mayankasar <mayankasar16@gmail.com>
2017-07-28 00:15:02 +0530

    Reset

--- CommandPrompt/src/main/Main.java
+++ CommandPrompt/src/main/Main.java
@@ -1,4 +1,3 @@
-
 package main;
 
 import java.io.*;
@@ -7,29 +6,87 @@ public class Main
 {
     public static void main(String args[])
     {
-        try{
-            ProcessBuilder builder = new ProcessBuilder("cmd.exe");
-            Process p = builder.start();
+        execute("date");
+    }
+    
+    public static void execute(String cmd)
+    {
+        try
+        {
+            System.out.println("java>"+cmd);
+            Process p = Runtime.getRuntime().exec("cmd.exe /c "+cmd);
             
+            StreamReader error = new StreamReader(p.getErrorStream());
+            StreamReader output = new StreamReader(p.getInputStream());
+            StreamWriter input = new StreamWriter(p.getOutputStream());
             
-            PrintWriter out = new PrintWriter(p.getOutputStream());
-            out.println("dir");
-            out.flush();
-            
-            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            Scanner sc = new Scanner(r);
-            String line;
-            while (sc.hasNext()) {
-                line = sc.nextLine();
-                System.out.println(line);
-            }
-            
-            out.println("exit");
-            out.flush();
-            out.close();
-            sc.close();
+            output.start();
+            error.start();
+            input.start();            
         }
         catch(Exception e){
+            System.out.println("Error:"+e);
+        }            
+    }
+}
+
+class StreamReader extends Thread
+{
+    InputStream in;
+    
+    public StreamReader(InputStream in){
+        this.in = in;
+    }
+    
+    @Override
+    public void run()
+    {
+        try
+        {
+            BufferedReader bis = new BufferedReader(new InputStreamReader(in));
+         
+            int ch;
+            while( (ch=bis.read()) != -1 )
+            {
+                System.out.print((char)ch);
+            } 
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error:"+e);
+        }
+    }
+}
+
+class StreamWriter extends Thread
+{
+    OutputStream out;
+    
+    public StreamWriter(OutputStream out){
+        this.out = out;
+    }
+    
+    @Override
+    public void run()
+    {
+        try
+        {
+            /**
+             * Take Input from KeyBoard and Print to Output Stream
+             */
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter writer = new PrintWriter(out);
+            
+            String line = null;
+            while ( (line = br.readLine()) != null)
+            {
+                writer.write(line);                              
+            }
+                
+            writer.close();            
+        }
+        catch(Exception e)
+        {
             System.out.println("Error:"+e);
         }
     }
